d9867c50ae884eb6bae7bf84ea6e50abae0c66a2
Kiana Long <kianajade42.kl@gmail.com>
2021-02-02 18:53:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,4 +13,12 @@ def initialize(name)
   def grade(student_grade)
     roster[student_grade]
   end
+  def sort
+    def sort
+        sorted = {}
+        roster.each do |grade, students|
+          sorted[grade] = students.sort
+        end
+        sorted
+    end
 end
